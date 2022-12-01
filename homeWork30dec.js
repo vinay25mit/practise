e5d5a16828c2492dd612c0233a8d8d17df76d225
Vinay Kumar <61008587+vinay25mit@users.noreply.github.com>
2022-12-01 13:35:36 +0530

    Add files via upload

---
+++ homeWork30dec.js
@@ -0,0 +1,41 @@
+const { number } = require("mathjs");
+const { Collection } = require("mongodb");
+const mongoose = require("mongoose");
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/inventory")
+//   .then((db) => console.log("connected to db "))
+//   .catch((err) => console.log("not connected"));
+
+
+//   const mongoose=require('mongoose');
+const userSchema=new mongoose.Schema({
+      id:{
+            type:String,
+            required: true
+      },
+      product:{
+            type:String,
+            required: true
+      },
+      manufacture:{
+            type:String,
+            required:true
+      },
+      price:{
+            type:number
+      }
+});
+const User = new mongoose.model("orders", userSchema);
+async function run() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/inventory");
+    console.log("connected to db");
+    const docs = await User.find({});
+    await docs.forEach((doc) => console.log(doc));
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await mongoose.disconnect();
+  }
+}
+run();
